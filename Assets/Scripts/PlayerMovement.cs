18f39b61e4cf732c5c99034e84eda9aa8d07d940
Roman Lepesh <rlepesh@mail.ru>
2022-02-02 16:24:41 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Animator animator;
+
+    public float speed = 5f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public float ___shift = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundmask;
+
+    Vector3 velocity;
+    bool isGrounded;
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundmask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && isGrounded)
+        {
+            speed = 15f;
+            animator.SetBool("isRunning", true);
+            //Camera.fieldOfView = Mathf.Lerp(Camera.fieldOfView, sprintFov, 5 * Time.deltaTime);
+        }
+        else
+        {
+            speed = 5f;
+            animator.SetBool("isRunning", false);
+        }
+       
+        if(velocity.y > 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
